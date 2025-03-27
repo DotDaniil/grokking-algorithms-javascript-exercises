580e204b1e3c7c58f4b6a8a885acc54a944798c7
DotDaniil <daniil-tikh@mail.ru>
2025-03-27 13:16:25 +0300

    add 7-1

--- 7-1-dijkstra-algorithm-javascript-realisation.js
+++ 7-1-dijkstra-algorithm-javascript-realisation.js
@@ -16,4 +16,51 @@ graph["fin"] = {};
 
 console.log(graph);
 
-const findLowestCostNode = (costs) => {};
+const infinity = Infinity;
+costs["a"] = 6;
+costs["b"] = 2;
+costs["fin"] = infinity;
+
+parents["a"] = "start";
+parents["b"] = "start";
+parents["fin"] = null;
+
+const findLowestCostNode = (costs) => {
+	let lowestCost = infinity;
+	let lowestCostNode = null;
+
+	for (const node in costs) {
+		const cost = costs[node];
+		if (cost < lowestCost && !processed.includes(node)) {
+			lowestCost = cost;
+			lowestCostNode = node;
+		}
+	}
+
+	return lowestCostNode;
+};
+
+const findDijkstra = () => {
+	let node = findLowestCostNode(costs);
+
+	while (node !== null) {
+		const cost = costs[node];
+		const neighbors = graph[node];
+
+		for (const neighbor in neighbors) {
+			const newCost = cost + neighbors[neighbor];
+			if (costs[neighbor] > newCost) {
+				costs[neighbor] = newCost;
+				parents[neighbor] = node;
+			}
+		}
+
+		processed.push(node);
+		node = findLowestCostNode(costs);
+	}
+
+	return { costs, parents };
+};
+
+console.log("Before Dijkstra:", costs);
+console.log("Resul Dijkstra:" findDijkstra());
