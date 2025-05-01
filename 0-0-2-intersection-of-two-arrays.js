6fcf0159518a318ac39702c37ce2662f236ccb4f
DotDaniil <daniil-tikh@mail.ru>
2025-05-01 09:28:23 +0300

    add 0-0-2

--- 0-0-2-intersection-of-two-arrays.js
+++ 0-0-2-intersection-of-two-arrays.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
 const intersection = (nums1, nums2) => {
   if (nums1.length === 0 || nums2.length === 0) return []; // Пустые массивы → нет пересечения
   
